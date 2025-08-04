9dea401d0c05189e6fe7ccb2b01b7011fc9e4ee6
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-08-04 18:29:35 +0530

    chore: add user model

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,50 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { constants } from "../constants/index.ts";
+
+export interface UserType extends Document {
+    firstName: string,
+    lastName: string,
+    address: mongoose.Types.ObjectId,
+    email: string,
+    password: string,
+    phoneNumber: string,
+    userType: number
+}
+
+const userSchema: Schema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Address",
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    userType: {
+        type: Number,
+        default: constants.USER_TYPES.USER,
+        enum: [constants.USER_TYPES.ADMIN, constants.USER_TYPES.USER]
+    }
+});
+
+export const User = mongoose.model<UserType>('User', userSchema);
+
