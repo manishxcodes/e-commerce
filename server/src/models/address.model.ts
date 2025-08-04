ceee28db3bf0ea43faee14e4287a2e3994cd8673
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-08-04 18:02:40 +0530

    chore: add address model

---
+++ server/src/models/address.model.ts
@@ -0,0 +1,42 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+//Address interface: Defines the TypeScript type for a Address document.
+//It extends Mongoose’s Document so we get MongoDB-specific features like _id.
+
+export interface AddressType extends Document {
+    houseNumber: string,
+    lane: string,
+    city: string,
+    district: string,
+    state: string,
+    pincode: string 
+}
+
+const addressSchema: Schema = new Schema({
+    houseNumber: {
+        type: String,
+        required: true
+    },
+    lane: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    district: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    pincode: {
+        type: String,
+        required: true
+    }
+});
+
+export const  Address =  mongoose.model<AddressType>('Address', addressSchema);
