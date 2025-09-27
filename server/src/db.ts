ef34d1a26842269ef4e36a226edc17f45e5667cc
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-09-27 23:07:58 +0530

    add db.ts

---
+++ server/src/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export const connectToDB = async () => {
+    try {
+        const connectionString = process.env.MONGO_URI;
+        if(connectionString) {
+            const connectionInstance = await mongoose.connect(`${connectionString}/e-comm`);  
+            console.log(`\n Database connected: \n DB host: ${connectionInstance.connection.host}`);
+        } else {
+            throw new Error("MongoDB connection string is missing")
+        }
+    } catch(err) {
+        console.log("Mongo DB connection failed", {details: err});
+        process.exit(1);
+    }
+}
