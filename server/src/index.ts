7357812d53474b8f54be6f466e747c2b20c840a9
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-09-27 23:08:48 +0530

    chore: handle db connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import { connectToDB } from './db.ts'
 
 const app = express();
 dotenv.config();
@@ -8,6 +9,18 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log("listening on port 3000");
+connectToDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error while listening", {details: error})
+    });
+
+    app.listen(port, () => {
+        console.log(`server running on port: ${port}`);
+    })
 })
+.catch((err) => {
+    console.log("Mongo DB connection failed \n", {details: err},);
+});
+
+
