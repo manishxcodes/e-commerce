546dea601f6a0e9fd93e920663bf0fe5b5379ada
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-11-01 22:05:29 +0530

    chore: add error handling middleware

---
+++ server/src/middlewares/errorHandler.ts
@@ -0,0 +1,25 @@
+import type { Request, Response, NextFunction } from "express";
+import { AppError } from '../utils/AppError.ts';
+
+export const errorHandler = (
+    err: Error, 
+    req: Request, 
+    res: Response,
+    next: NextFunction
+) => {
+    console.log('Error: ', err);
+
+    // handle custom error 
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            success: false,
+            message: err.message,
+        });
+    }
+
+    // default error 
+    return res.status(500).json({
+        success: false,
+        message: err.message || "Internal Server Error",
+    });
+}
