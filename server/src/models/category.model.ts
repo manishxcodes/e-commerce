c2ac85a2edd978d37c9c7edfe3825eacb4dd74ce
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-11-03 19:14:34 +0530

    chore: add image, gender and parent category field

--- server/src/models/category.model.ts
+++ server/src/models/category.model.ts
@@ -3,6 +3,8 @@ import mongoose, { Schema, Document} from 'mongoose';
 export interface ICategory extends Document {
     name: string,
     imageUrl?: string,
+    parent?: mongoose.Types.ObjectId,
+    gender?: "men" | "women" | ""
 }
 
 const categorySchema = new Schema<ICategory>({
@@ -12,6 +14,18 @@ const categorySchema = new Schema<ICategory>({
         unique: true,
         required: true,
         maxlength: 20
+    },
+    imageUrl: {
+        type: String
+    },
+    parent: {
+        type: Schema.Types.ObjectId,
+        ref: "Category"
+    },
+    gender: {
+        type: String, 
+        enum: ["men", "women", ""],
+        default: ""
     }
 }, {timestamps: true});
 
