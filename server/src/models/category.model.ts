8da11486b20c39668829d47360c9282e3d0b8488
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-11-04 23:54:35 +0530

    refactor: rename field parent to parentCategory and make index name, gender and parentCategory

--- server/src/models/category.model.ts
+++ server/src/models/category.model.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema, Document} from 'mongoose';
 export interface ICategory extends Document {
     name: string,
     imageUrl?: string,
-    parent?: mongoose.Types.ObjectId,
+    parentCategory?: mongoose.Types.ObjectId,
     gender?: "men" | "women" | ""
 }
 
@@ -11,14 +11,13 @@ const categorySchema = new Schema<ICategory>({
     name: {
         type: String,
         trim: true,
-        unique: true,
         required: true,
         maxlength: 20
     },
     imageUrl: {
         type: String
     },
-    parent: {
+    parentCategory: {
         type: Schema.Types.ObjectId,
         ref: "Category"
     },
@@ -28,5 +27,8 @@ const categorySchema = new Schema<ICategory>({
         default: ""
     }
 }, {timestamps: true});
+
+categorySchema.index({ name: 1, gender: 1, parentCategory: 1 }, { unique: true });
+
 
 export const Category = mongoose.model<ICategory>("Category", categorySchema);
