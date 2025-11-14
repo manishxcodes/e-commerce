1d06e913e473fb44e67aeb526defeb26f81e235d
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-11-14 16:20:33 +0530

    add otp route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import userRouter from '../routes/user.routes.ts'
+import userRouter from './user.route.ts'
+import otpRouter from './otp.route.ts'
 
 const router = Router();
 
 router.use('/user', userRouter);
+
+router.use('/otp', otpRouter)
 
 
 export default router;
