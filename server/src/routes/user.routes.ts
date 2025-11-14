52cd3151fe6c49d8e2df0b11429343b3be356355
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-11-14 16:18:47 +0530

    rename user.routes.ts to user.route.ts

--- server/src/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import userController from "controllers/user.controller.ts";
-import { Router } from "express";
-
-const router = Router();
-
-router.get('/:id' , userController.getUserById);
-router.post('/signup', userController.signup);
-
-export default router;
