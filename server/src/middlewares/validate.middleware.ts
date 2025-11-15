2b8d3be2079e98b908b12f2452aae5137e4261f5
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-11-15 18:53:45 +0530

    fix: add ZodType,  remove ZodSchema and format error message

--- server/src/middlewares/validate.middleware.ts
+++ server/src/middlewares/validate.middleware.ts
@@ -1,23 +1,23 @@
 import type { Response, Request, NextFunction } from "express";
 import { AppError } from "utils/AppError.ts";
-import { ZodSchema } from "zod/v3";
+import { ZodType } from "zod";
 
 export const validate = (
-    schema: ZodSchema
+    schema: ZodType
 ) => (
     req: Request, res: Response, next: NextFunction
 ) => {
     const result = schema.safeParse(req.body);
     if(!result.success) {
-        const errorMessage =  result.error.errors.map((err) => ({
+        const errorMessage =  result.error.issues.map((err) => ({
             path: err.path.join("."),
             message: err.message,
         }));
 
-        
+        console.log("error: ", errorMessage);
         return next(
-            new AppError(`Validation failed: ${JSON.stringify(errorMessage)}`, 400)
-        );
+            new AppError(`Validation failed:`, 400, errorMessage)
+        ); 
     }
 
     req.body = result.data;
