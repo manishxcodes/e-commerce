85d338b943ab7baf12a8d39ca20c619640050660
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-11-22 00:07:46 +0530

    add regiser, login router

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,14 +1,16 @@
-import { findUserByEmail, login } from "controllers/user.controller.ts";
+import { findUserByEmail, login, logout, registerUser } from "controllers/user.controller.ts";
 import { Router } from "express";
 import { isAdmin } from "middlewares/admin.middleware.ts";
 import { authMiddleware } from "middlewares/auth.middleware.ts";
 import { isUser } from "middlewares/user.middleware.ts";
 import { validate } from "middlewares/validate.middleware.ts";
-import { signinSchema } from "schema.ts";
+import { loginSchema, registerSchema } from "schema.ts";
 
 const router = Router();
 
+router.post('/register', validate(registerSchema), registerUser);
+router.post('/login', validate(loginSchema), login);
+router.post('/logout', authMiddleware, logout);
 router.get('/:email', authMiddleware, findUserByEmail);
-router.post('/login', validate(signinSchema), login);
 
 export default router;
