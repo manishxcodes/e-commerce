4d40a45741a52058d88fd87d300e45ff8e7e3295
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-11-22 12:01:31 +0530

    add upload image route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import userRouter from './user.route.ts'
-import otpRouter from './otp.route.ts'
+import userRouter from './user.route.ts';
+import otpRouter from './otp.route.ts';
+import uploadRouter from './upload.route.ts';
 
 const router = Router();
 
 router.use('/user', userRouter);
-
-router.use('/otp', otpRouter)
+router.use('/otp', otpRouter);
+router.use('/upload', uploadRouter);
 
 
 export default router;
