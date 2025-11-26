cf79b54ae29f7efff788def5ee3ddcb8ac2e07f0
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-11-26 23:45:31 +0530

    add multer middleware

---
+++ server/src/middlewares/multer.middleware.ts
@@ -0,0 +1,22 @@
+import multer from 'multer';
+import type { Request } from 'express';
+import { AppError } from 'utils/AppError.ts';
+
+const storage = multer.memoryStorage();
+
+// file filter
+const imageFileFilter = (req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
+    const allowedMimeType = ['image/jpeg', 'image/jpg', 'image/png'];
+
+    if(allowedMimeType.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new AppError('Only image files [jpeg, jpg, png] are allowed', 400));
+    }
+};
+
+export const uploadImageMiddleware = multer({
+    storage, 
+    fileFilter: imageFileFilter,
+    limits: { fileSize: 2 * 1024 * 1024 },
+});
