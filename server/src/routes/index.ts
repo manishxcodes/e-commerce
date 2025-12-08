ecf485dd7a9c549a03528500baf48cb5f1e77705
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-08 23:56:06 +0530

    fix: rename uploadRouter to 'productRouter'

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import userRouter from './user.route.ts';
 import otpRouter from './otp.route.ts';
-import uploadRouter from './product.route.ts';
+import productRouter from './product.route.ts';
 import addressRouter from './address.route.ts';
 
 const router = Router();
 
 router.use('/user', userRouter);
 router.use('/otp', otpRouter);
-router.use('/upload', uploadRouter);
+router.use('/upload', productRouter);
 router.use('/address', addressRouter)
 
 
