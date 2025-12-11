acff0f2147de5178950ad822829e89e64e9fd610
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-11 23:39:22 +0530

    refactor: rename 'getProduct' to 'getProductById'

--- server/src/routes/product.route.ts
+++ server/src/routes/product.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { uploadImageMiddleware } from "middlewares/multer.middleware.ts";
-import { addProduct, getAllProducts, getProduct, updateProduct, deleteProduct } from '../controllers/product.controller.ts';
+import { addProduct, getAllProducts, getProductById, updateProduct, deleteProduct } from '../controllers/product.controller.ts';
 import { authMiddleware } from "middlewares/auth.middleware.ts";
 import { isAdmin } from "middlewares/admin.middleware.ts";
 import { validate } from "middlewares/validate.middleware.ts";
@@ -10,7 +10,7 @@ import { addProductSchema, updateProductSchema } from "schema.ts";
 const router = Router();
 
 router.get('/', getAllProducts);
-router.get('/:id', getProduct);
+router.get('/:id', getProductById);
 
 router.post('/', authMiddleware, isAdmin, uploadImageMiddleware.single('photo'), validate(addProductSchema), addProduct);
 router.put('/:id', authMiddleware, isAdmin, uploadImageMiddleware.single('photo'), validate(updateProductSchema), updateProduct);
