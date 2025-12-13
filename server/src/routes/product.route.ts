c3ad0583dba97a863e59e35f64f1effc5699cf02
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-13 23:09:21 +0530

    add route to update product stock

--- server/src/routes/product.route.ts
+++ server/src/routes/product.route.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { uploadImageMiddleware } from "middlewares/multer.middleware.ts";
-import { addProduct, getAllProducts, getProductById, updateProduct, deleteProduct } from '../controllers/product.controller.ts';
+import { addProduct, getAllProducts, getProductById, updateProduct, deleteProduct, updateProductStock } from '../controllers/product.controller.ts';
 import { authMiddleware } from "middlewares/auth.middleware.ts";
 import { isAdmin } from "middlewares/admin.middleware.ts";
 import { validate } from "middlewares/validate.middleware.ts";
-import { addProductSchema, updateProductSchema } from "schema.ts";
+import { addProductSchema, updateProductSchema, updateProductStockSchema } from "schema.ts";
 
 
 const router = Router();
@@ -15,9 +15,6 @@ router.get('/:id', getProductById);
 router.post('/', authMiddleware, isAdmin, uploadImageMiddleware.single('photo'), validate(addProductSchema), addProduct);
 router.put('/:id', authMiddleware, isAdmin, uploadImageMiddleware.single('photo'), validate(updateProductSchema), updateProduct);
 router.delete('/:id', authMiddleware, isAdmin, deleteProduct);
-
-router.get('/image', (req, res) => {
-    res.status(200).send("file recieved");
-});
+router.put('/update-stock/:id', authMiddleware, isAdmin, validate(updateProductStockSchema), updateProductStock)
 
 export default router;
