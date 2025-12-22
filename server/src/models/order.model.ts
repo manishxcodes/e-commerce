0a867065286f97823c61d658fe9fcfbaf1cb1e5c
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-23 00:39:49 +0530

    add order model

--- server/src/models/order.model.ts
+++ server/src/models/order.model.ts
@@ -1,67 +1,135 @@
-import mongoose, { Document, Schema } from "mongoose";
-import type { ICartItem } from "./cartItem.model.ts";
-import type { IUser } from "./user.model.ts";
+import mongoose, { Document, mongo, Schema } from "mongoose";
 import { constants } from "../constants/index.ts"
 
-export interface IOrder extends Document {
-    orderId: string,
-    products: ICartItem[],
-    user: IUser,
-    totalAmount: string,
-    subTotal: string,
-    discount: string,
-    shippingFee: string,
-    paymentMode: string,
-    paymentId: string,
-    status: string,
+export interface IOrderItem {
+    product: mongoose.Types.ObjectId,
+    productData: {
+        name: string,
+        imageKey: string, 
+        brand: string
+    },
+    quantity: number,
+    size: string,
+    price: number,
+    subTotal: number
 }
 
-const orderSchema: Schema = new mongoose.Schema<IOrder>({
-    orderId: {
-        type: String,
-        required: true,
+export interface IOrder extends Document {
+    orderNumber: string,
+    items: IOrderItem[],
+    user: mongoose.Types.ObjectId,
+    shippingAddress: {
+        firstName: string,
+        lastName: string,
+        addressLine1: string,
+        addressLine2?: string,
+        city: string,
+        district: string,
+        state: string,
+        pincode: string,
+        phoneNumber: string
+    },
+    pricing: {
+        subtotal: number, 
+        discount: number, 
+        shippingFee: number, 
+        total: number,
+    },
+    payment: {
+        method: string,
+        status: string, 
+        razorpayOrderId?: string, 
+        razorpayPaymentId?: string,
+        razorpaySignature?: string,
+        paidAt?: Date,
+    }, 
+    status: {
+        status: string,
+        deliveryDate?: Date,
+        cancelledAt?: Date, 
+        cancellationReason?: string
+    }
+}
+
+const orderSchema = new Schema<IOrder>({
+    orderNumber: {
+        type: String, 
+        required: true, 
         unique: true,
         index: true
     },
-    products: [{
-        type: mongoose.Schema.ObjectId,
-        ref: "CartItem"
+    items: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true
+        },
+        productData: {
+            name: { type: String, required: true },
+            imageKey: { type: String, required: true },
+            brand: { type: String, required: true }
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1
+        },
+        size: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number, 
+            required: true,
+            min: 0
+        },
+        subtotal: {
+            type: Number,
+            required: true,
+            min: 0
+        }
     }],
     user: {
-        type: mongoose.Schema.ObjectId,
+        type: mongoose.Schema.Types.ObjectId,
         ref: "User",
+        required: true,
         index: true
     },
-    totalAmount: {
-        type: String,
-        required: true,
+    shippingAddress: {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        addressLine1: { type: String, required: true },
+        addressLine2: { type: String },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        pincode: { type: String, required: true },
+        phoneNumber: { type: String, required: true }
     },
-    subTotal: {
-        type: String,
-        required: true,
+    pricing: {
+        subtotal: { type: Number, required: true, min: 0 },
+        discount: { type: Number, default: 0, min: 0 },
+        shippingFee: { type: Number, default: 0, min: 0 },
+        total: { type: Number, required: true, min: 0 }
     },
-    discount: {
-        type: String,
-        required: true,
-    },
-    shippingFee: {
-        type: String,
-        required: true,
-    },
-    paymentMode: {
-        type: String,
-        enum: Object.values(constants.PAYMENT_MODE),
-        required: true
-    },
-    paymentId: {
-        type: String,
-        required: true,
-        unique: true,
+    payment: {
+        method: {
+            type: String, 
+            enum: Object.values(constants.PAYMENT_MODE),
+            required: true
+        },
+        status: {
+            type: String,
+            enum:Object.values(constants.PAYMENT_STATUS) ,
+            default: constants.PAYMENT_STATUS.PENDING
+        },
+        razorpayOrderId: String,
+        razorpayPaymentId: String, 
+        razorpaySignature: String, 
+        paidAt: Date, 
     },
     status: {
-        type: String,
-        enum: Object.values(constants.ORDER_STATUS)
+        type: String, 
+        enum: Object.values(constants.ORDER_STATUS),
+        
     }
-}, {timestamps: true});
-
-export const Order = mongoose.model<IOrder>("Order", orderSchema);
+})
