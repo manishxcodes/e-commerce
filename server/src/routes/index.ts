f44be292a303e4d611a746a028c40cf3f15bcfe2
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-23 02:01:45 +0530

    add cart router for handling cart operation

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,13 +3,15 @@ import userRouter from './user.route.ts';
 import otpRouter from './otp.route.ts';
 import productRouter from './product.route.ts';
 import addressRouter from './address.route.ts';
+import cartRouter from './cart.route.ts'
 
 const router = Router();
 
 router.use('/user', userRouter);
 router.use('/otp', otpRouter);
 router.use('/product', productRouter);
-router.use('/address', addressRouter)
+router.use('/address', addressRouter);
+router.use('/cart', cartRouter);
 
 
 export default router;
